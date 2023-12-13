2659987025b882fbd6e1c03c1f272cebd5a10a17
wowroberto <rcastillo1@woolworths.com.au>
2023-12-13 12:07:18 +1100

    folder renamed

---
+++ Create-Random-Petname-StoAcct/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  default     = "australiaeast"
+  description = "Location where resources will be created"
+}
+
+
+variable "azurerm_resource_group" {
+  default = "RGAADX"
+}
