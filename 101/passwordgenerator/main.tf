77f12e241403698787557e9180d779e8e3423fd5
Roberto <roberto@rober.to>
2024-12-13 11:33:15 +1100

    Add random password generator and reintroduce pet name storage account configuration

--- 101/passwordgenerator/main.tf
+++ 101/passwordgenerator/main.tf
@@ -7,6 +7,11 @@ resource "random_password" "password" {
   length           = var.length
   special          = true
   override_special = "!#$%&*()-_=+[]{}<>:?"
+
+   keepers = {
+    # this will recreate the password if the timestamp changes: meaning every running will generate a new password
+    timestamp = "${timestamp()}"
+  }
 }
 
 output "espassword" {
