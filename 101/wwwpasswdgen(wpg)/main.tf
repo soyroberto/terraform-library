53d98fece772192abf8c8a3a9d81b44cc33db0a5
Roberto <roberto@rober.to>
2025-01-08 16:05:17 +1100

    Remove obsolete comments from main.tf for random password generation

--- 101/wwwpasswdgen(wpg)/main.tf
+++ 101/wwwpasswdgen(wpg)/main.tf
@@ -18,19 +18,3 @@ output "espassword" {
   value     = random_password.password.result
   sensitive = true
 }
-
-/*  Create a random password with the following requirements:
-  - Length: 18 default value
-  to change the length, use the following command
-  export TF_VAR_length=20 in the shell changing the lenght will regenerate the password
-   
-   Can also be done using the following command: terraform apply -var 'length=20' -auto-approve
-  - Special characters: !#$%&*()-_=+[]{}<>:?
-  - Output the password as a sensitive output
-  to show the password in the Terraform output
-  terraform output espassword
-  to regenerate the password
-    terraform taint random_password.password
-    terraform apply
-    terraform output espassword
-*/
