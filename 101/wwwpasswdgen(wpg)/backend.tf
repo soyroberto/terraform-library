a5868e62222b5bc4c25d50c9d64b4c9636f8e62b
Roberto López <roberto@rober.to>
2025-01-09 16:08:53 +1100

    Update backend.tf

--- 101/wwwpasswdgen(wpg)/backend.tf
+++ 101/wwwpasswdgen(wpg)/backend.tf
@@ -17,10 +17,10 @@ terraform {
 provider "azurerm" {
   features {}
 
-  client_id       = "a4b10b4c-c8c0-411d-a566-eb330fe6c8bd"
+  client_id       = ""
   client_secret   = var.client_secret
-  tenant_id       = "b25f0c4b-3037-4565-a787-0b8b56a49970"
-  subscription_id = "ff8e65ab-9922-40f7-a46c-610fd088a25c"
+  tenant_id       = ""
+  subscription_id = ""
 }
 variable "client_secret" {
     type = string
